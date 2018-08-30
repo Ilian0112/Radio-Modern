5e44272abbea1f3268a70186873e0814ffbd707e
Radio Modern <37845826+modernradio@users.noreply.github.com>
2018-08-30 21:00:50 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -10,21 +10,21 @@ bot.on("ready", () => {
 
 function autoplayradio () {
 
-    var channels_autoplayradio = ["482530580123222044", "480886933115895809"]
-    //                           BAR                     Imaginarium
+    var channels_autoplayradio = ["481202105382862848", "444201909666971648", "442651081080569867"]
+    //                           Ilian                  SupersFanne           Le QZ
 
     autoplayradio_join();
 
     function autoplayradio_join () {
         var i;
         for (i = 0; i < channels_autoplayradio.length; i++) {
-            var channels_autoplayradio_find = bot.channels.find("id", channels_autoplayradio[i]);
+            var channels_autoplayradio_find = bot.channels.findAll("id", channels_autoplayradio[i]);
             channels_autoplayradio_find.join().then(connection => {
                 require("http").get("http://streaming.radionomy.com/RadioModern", (res) => {
                     connection.playStream(res);
                 })
             })
-            console.log("-> autojoin\n    - Salon \"" + channels_autoplayradio[i].name + "\" (" + channels_autoplayradio[i].guild.name + ")\n" + separation)    
+            console.log("-> autojoin\n    + Salon \"" + channels_autoplayradio_find.name + "\" (" + channels_autoplayradio_find.guild.name + ")\n" + separation)
         }
         setTimeout(autoplayradio_leave, 15 * 60 * 1000)
     }
@@ -34,7 +34,7 @@ function autoplayradio () {
         for (i = 0; i < channels_autoplayradio.length; i++) {
             var channels_autoplayradio_find = bot.channels.findAll("id", channels_autoplayradio[i]);
             channels_autoplayradio_find.leave();
-            console.log("-> autojoin\n    + Salon \"" + channels_autoplayradio[i].name + "\" (" + channels_autoplayradio[i].guild.name + ")\n" + separation)
+            console.log("-> autojoin\n    - Salon \"" + channels_autoplayradio_find.name + "\" (" + channels_autoplayradio_find.guild.name + ")\n" + separation)
         }
         autoplayradio_join();
     }
