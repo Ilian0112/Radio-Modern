c43bf6fed82caef4dc3718372e3ae0e9c711eb57
Radio Modern <37845826+modernradio@users.noreply.github.com>
2018-11-20 18:28:18 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,10 +4,10 @@ var bot = new Discord.Client();
 
 var separation = "><><><><><><><><><><><";
 
-var cooldown = new Set();
+var prefix = ".";
 
-var channels_autoradio = [/*"442651081080569867", "499601814656909321", */"482530580123222044", "480886933115895809"];
-//                        Le QZ                 Omega                 BAR                   Imaginarium
+var channels_autoradio = ["442651081080569867", "499601814656909321", "480886933115895809"];
+//                        Le QZ                 Omega                 Imaginarium
 
 bot.on("ready", () => {
     var embed = new Discord.RichEmbed()
@@ -16,50 +16,43 @@ bot.on("ready", () => {
     bot.channels.findAll("name", "logs-radio").map(c => c.send(embed));
     console.log(separation + "\nBot prêt\n" + separation);
     autoradio_join();
-    autoradio_test();
+});
+
+bot.on("message", (message) => {
+    if (message.author.id === "323039726040776705" || message.author.id === "182977157314772993") {
+        if (!message.content.startsWith(prefix)) return;
+        var splited = message.content.substring(prefix.length).split(" ");
+        var command = splited[0];
+        switch (command.toLowerCase()) {
+            case "autoradio":
+                message.delete();
+                console.log("-> " + prefix + "autoradio\n" + separation);
+                autoradio_leave();
+                break;
+        }
+    }
 });
 
 function autoradio_join () {
     for (var i = 0; i < channels_autoradio.length; i++) {
         var channels_autoradio_find = bot.channels.find("id", channels_autoradio[i]);
-        channels_autoradio_find.join();
+        channels_autoradio_find.join().then(connection => {
+            require("http").get("http://streaming.radionomy.com/RadioModern", (res) => {
+                connection.playStream(res)
+            })
+        })
+        console.log("-> autoradio (join)\n    - Salon \"" + channels_autoradio_find.name + "\" (" + channels_autoradio_find.guild.name + ")\n" + separation);
     }
+    setTimeout(autoradio_leave, 20 * 60 * 1000);
 }
 
-function autoradio_test () {
+function autoradio_leave () {
     for (var i = 0; i < channels_autoradio.length; i++) {
         var channels_autoradio_find = bot.channels.find("id", channels_autoradio[i]);
-        var channels_autoradio_find_members_array = channels_autoradio_find.members.array();
-        if (channels_autoradio_find_members_array.length > 1) {
-            autoradio_play(channels_autoradio[i]);
-            setCooldown(channels_autoradio[i]);
-        }
-        if (channels_autoradio_find_members_array.length == 1) return autoradio_stop(channels_autoradio[i]);
+        channels_autoradio_find.leave();
+        console.log("-> autoradio (leave)\n    + Salon \"" + channels_autoradio_find.name + "\" (" + channels_autoradio_find.guild.name + ")\n" + separation);
     }
-    setTimeout(autoradio_test, 10000)
-}
-
-function autoradio_play (id) {
-    if (cooldown.has(id)) return;
-    var channels_autoradio_find = bot.channels.find("id", id);
-    channels_autoradio_find.join().then(connection => {
-        require("http").get("http://streaming.radionomy.com/RadioModern", (res) => {
-            connection.playStream(res)
-        })
-    })
-}
-
-function autoradio_stop (id) {
-    var channels_autoradio_find = bot.channels.find("id", id);
-    channels_autoradio_find.leave();
-    channels_autoradio_find.join();
-}
-
-function setCooldown (id) {
-    cooldown.add(id);
-    setTimeout(() => {
-        cooldown.delete(id);
-    }, 5000);
+    autoradio_join();
 }
 
 bot.login(process.env.TOKEN);
