9e4d4e812514ae19b77f1efe577e89daf2482cf2
Radio Modern <37845826+modernradio@users.noreply.github.com>
2018-12-08 15:29:12 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -13,7 +13,7 @@ bot.on("ready", () => {
     var embed = new Discord.RichEmbed()
         .setTitle("RM1 - Je suis connecté")
         .setTimestamp()
-    bot.channels.findAll("name", "logs-radio").map(c => c.send(embed));
+    bot.channels.findAll("name", "logs-radiom").map(c => c.send(embed));
     console.log(separation + "\nBot prêt\n" + separation);
     autoradio_join();
     autoradio_test();
@@ -27,16 +27,16 @@ function autoradio_join () {
 }
 
 function autoradio_test () {
-    for (var i = 0; i < channels_autoradio.length; i++) {
-        var channels_autoradio_find = bot.channels.find("id", channels_autoradio[i]);
+    for (var j = 0; j < channels_autoradio.length; j++) {
+        var channels_autoradio_find = bot.channels.find("id", channels_autoradio[j]);
         var channels_autoradio_find_members_array = channels_autoradio_find.members.array();
         if (channels_autoradio_find_members_array.length > 1) {
-            autoradio_play(channels_autoradio[i]);
-            setCooldown(channels_autoradio[i]);
+            autoradio_play(channels_autoradio[j]);
+            setCooldown(channels_autoradio[j]);
         }
-        if (channels_autoradio_find_members_array.length == 1) return autoradio_stop(channels_autoradio[i]);
+        if (channels_autoradio_find_members_array.length == 1) return autoradio_stop(channels_autoradio[j]);
     }
-    setTimeout(autoradio_test, 10000)
+    setTimeout(autoradio_test, 1000)
 }
 
 function autoradio_play (id) {
