7fdbbef6e39a4282b3da414ef8b22aff6218c629
Radio Modern <37845826+modernradio@users.noreply.github.com>
2019-01-17 18:56:25 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -9,10 +9,10 @@ var channels_autoradio = ["442651081080569867", "499601814656909321", "480886933
 
 bot.on("ready", () => {
     var embed = new Discord.RichEmbed()
-        .setTitle("RM1 - Je suis connecté")
+        .setTitle("RM1 - I'm connected")
         .setTimestamp()
     bot.channels.findAll("name", "logs-radiom").map(c => c.send(embed));
-    console.log(separation + "\nBot prêt\n" + separation);
+    console.log(separation + "\nBot ready\n" + separation);
     autoradio_join();
 });
 
@@ -24,7 +24,7 @@ function autoradio_join () {
                 connection.playStream(res)
             })
         })
-        console.log("-> autoradio (join)\n    - Salon \"" + channels_autoradio_find.name + "\" (" + channels_autoradio_find.guild.name + ")\n" + separation);
+        console.log("-> autoradio (join)\n    + Salon \"" + channels_autoradio_find.name + "\" (" + channels_autoradio_find.guild.name + ")\n" + separation);
     }
     setTimeout(autoradio_leave, 20 * 60 * 1000);
 }
@@ -33,7 +33,7 @@ function autoradio_leave () {
     for (var i = 0; i < channels_autoradio.length; i++) {
         var channels_autoradio_find = bot.channels.find("id", channels_autoradio[i]);
         channels_autoradio_find.leave();
-        console.log("-> autoradio (leave)\n    + Salon \"" + channels_autoradio_find.name + "\" (" + channels_autoradio_find.guild.name + ")\n" + separation);
+        console.log("-> autoradio (leave)\n    - Salon \"" + channels_autoradio_find.name + "\" (" + channels_autoradio_find.guild.name + ")\n" + separation);
     }
     autoradio_join();
 }
