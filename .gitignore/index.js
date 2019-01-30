0923a88e77589e18b431553139f9a3ab495c8b69
Radio Modern <37845826+modernradio@users.noreply.github.com>
2019-01-30 14:34:57 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,7 +4,9 @@ var bot = new Discord.Client();
 
 var separation = "▬▬▬▬▬▬▬▬▬▬▬▬▬";
 
-var channels_autoradio = ["540149724607545355", "540149776641949710"];
+var prefix = process.env.prefix;
+
+var infoRecently_Join = new Set();
 
 bot.on("ready", () => {
     var embed = new Discord.RichEmbed()
@@ -15,43 +17,57 @@ bot.on("ready", () => {
     autoradio_join();
 });
 
+bot.on("message", (message) => {
+    var bot_member = bot.guilds.get("504206997164654602").members.get(message.author.id);
+    if (!bot_member.roles.has("538420571533475850")) return;
+    if (!message.content.startsWith(prefix)) return;
+    var splited = message.content.substring(prefix.length).split(" ");
+    var args0 = splited[0];
+    switch (args0.toLowerCase()) {
+        case "autoradio":
+            console.log("-> " + config.prefix + "autoradio\n" + separation);
+            autoradio_leave();
+            break;
+    }
+});
+
 function autoradio_join () {
-    for (var i = 0; i < channels_autoradio.length; i++) {
-        var channels_autoradio_found = bot.channels.find("id", channels_autoradio[i]);
-        channels_autoradio_found.join().then(connection => {
+    var channels_autoradio_list = process.env.channels_autoradio.split(",");
+    for (var i = 0; i < channels_autoradio_list.length; i++) {
+        var channel_autoradio = bot.channels.find("id", channels_autoradio_list[i]);
+        channel_autoradio.join().then(connection => {
             require("http").get("http://streaming.radionomy.com/RadioModern", (res) => {
                 connection.playStream(res)
             })
         })
-        console.log("+ Salon \"" + channels_autoradio_found.name + "\" (" + channels_autoradio_found.guild.name + ")\n" + separation);
+        console.log("+ Salon \"" + channel_autoradio.name + "\" (" + channel_autoradio.guild.name + ")\n" + separation);
     }
     setTimeout(autoradio_leave, 20 * 60 * 1000);
 }
 
 function autoradio_leave () {
-    for (var i = 0; i < channels_autoradio.length; i++) {
-        var channels_autoradio_found = bot.channels.array().filter(c => c.id === channels_autoradio[i]);
-        channels_autoradio_found.leave();
-        console.log("- Salon \"" + channels_autoradio_found.name + "\" (" + channels_autoradio_found.guild.name + ")\n" + separation);
+    var channels_autoradio_list = process.env.channels_autoradio.split(",");
+    for (var i = 0; i < channels_autoradio_list.length; i++) {
+        var channel_autoradio = bot.channels.find("id", channels_autoradio_list[i]);
+        channel_autoradio.leave();
+        console.log("- Salon \"" + channel_autoradio.name + "\" (" + channel_autoradio.guild.name + ")\n" + separation);
     }
     autoradio_join();
 }
 
-// var infoRecently_Join = new Set();
+bot.on("voiceStateUpdate", (oldMember, newMember) => {
 
-// bot.on("voiceStateUpdate", (oldMember, newMember) => {
+    var infoJoin_embed = new Discord.RichEmbed()
+        .setAuthor("Info", "https://cdn.discordapp.com/attachments/517737550618558466/534453800044462081/678110-sign-info-512.png")
+        .setDescription("Fantastic !\nYou have just joined a channel dedicated to Radio Modern !\n\nIf there is any problem, come [here](http://discord.gg/4fDkbPw)\nThe Radio Modern is a radio broadcasting remix ! If you want to support us, add me to your server [here](http://discordapp.com/oauth2/authorize?&client_id=444951082750312468&scope=bot&permissions=37055552)\nType .help for more information about me !\nEnjoy !")
+        .setColor("#2E9AFE")
 
-//     var infoJoin_embed = new Discord.RichEmbed()
-//         .setAuthor("Info", "https://cdn.discordapp.com/attachments/517737550618558466/534453800044462081/678110-sign-info-512.png")
-//         .setDescription("Fantastic !\nYou have just joined a channel dedicated to Radio Modern !\n\nIf there is any problem, come [here](http://discord.gg/4fDkbPw)\nThe Radio Modern is a radio broadcasting remix ! If you want to support us, add me to your server [here](http://discordapp.com/oauth2/authorize?&client_id=444951082750312468&scope=bot&permissions=37055552)\nType .help for more information about me !\nEnjoy !")
-//         .setColor("#2E9AFE")
-
-//     if (channels_autoradio.includes(newMember.voiceChannelID)) {
-//         if (infoRecently_Join.has(newMember.user.id) || oldMember.voiceChannelID === newMember.voiceChannelID) return;
-//         infoRecently_Join.add(newMember.user.id)
-//         newMember.send(infoJoin_embed)
-//     }
+    if (channels_autoradio.includes(newMember.voiceChannelID)) {
+        if (infoRecently_Join.has(newMember.user.id) || oldMember.voiceChannelID === newMember.voiceChannelID) return;
+        infoRecently_Join.add(newMember.user.id)
+        newMember.send(infoJoin_embed)
+    }
     
-// })
+})
 
-bot.login(process.env.TOKEN);
+bot.login(process.env.token);
