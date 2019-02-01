5475aa66d747350845209fe74e2daf7fd57ed79c
Radio Modern <37845826+modernradio@users.noreply.github.com>
2019-02-01 13:20:05 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -60,10 +60,10 @@ bot.on("voiceStateUpdate", (oldMember, newMember) => {
 
     var infoJoin_embed = new Discord.RichEmbed()
         .setAuthor("Info", "https://cdn.discordapp.com/attachments/517737550618558466/534453800044462081/678110-sign-info-512.png")
-        .setDescription("Fantastic !\nYou have just joined a channel dedicated to Radio Modern !\n\nIf there is any problem, come [here](http://discord.gg/4fDkbPw)\nThe Radio Modern is a radio broadcasting remix ! If you want to support us, add me to your server [here](http://discordapp.com/oauth2/authorize?&client_id=444951082750312468&scope=bot&permissions=37055552)\nType .help for more information about me !\nEnjoy !")
+        .setDescription("Fantastic !\nYou have just joined a channel dedicated to Radio Modern !\n\nIf there is any problem, come [here](http://discord.gg/4fDkbPw)\nThe Radio Modern is a radio broadcasting remix ! If you want to support us, add me to your server [here](http://discordapp.com/oauth2/authorize?&client_id=444951082750312468&scope=bot&permissions=37055552)\nType " + prefix + "help for more information about me !\nEnjoy !")
         .setColor("#2E9AFE")
 
-    if (process.env.channels_autoradio.split(",").includes(newMember.voiceChannelID) || oldMember.user.id !== bot.user.id) {
+    if (process.env.channels_autoradio.split(",").includes(newMember.voiceChannelID) && oldMember.user.id !== bot.user.id) {
         if (infoRecently_Join.has(newMember.user.id) || oldMember.voiceChannelID === newMember.voiceChannelID) return;
         infoRecently_Join.add(newMember.user.id)
         newMember.send(infoJoin_embed)
